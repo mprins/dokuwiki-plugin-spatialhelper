39db67a7f9bec3681f828ead4df8afd3e90d327b
Mark C. Prins <mprins@users.sf.net>
2011-03-10 22:58:07 +0000

    geohash spatial indexing works/function complete (still need to implement index locking)

--- helper/index.php
+++ helper/index.php
@@ -25,55 +25,52 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
 if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
 require_once DOKU_PLUGIN.'spatialhelper/Geohash.php';
-
 
 class helper_plugin_spatialhelper_index extends DokuWiki_Plugin {
 	/**
 	 * directory for index files
 	 * @var string
 	 */
-	var $idx_dir    = '';
+	var $idx_dir = '';
+
 	/**
-	 * spatial index, well lookup list so we can do spatial queries.
-	 * entries should be:
-	 * array("geohash" => ["id",])
+	 * spatial index, well lookup list/array so we can do spatial queries.
+	 * entries should be: array("geohash" => {"id1","id3",})
 	 * @var array
 	 */
-	var $spatial_idx  = array();
-
+	var $spatial_idx = array();
 
 	/**
-	 * Constructor
+	 * Constructor, initialises the spatial index.
 	 */
 	function helper_plugin_spatialhelper_index() {
 		dbglog('initialize','--- spatialhelper_index::helper_plugin_spatialhelper_index ---');
 		global $conf;
-		$this->idx_dir=$conf['indexdir'];
-
+		$this->idx_dir = $conf['indexdir'];
+		// test if there is an index, if not build one for the wiki
 		if (!@file_exists($this->idx_dir.'/spatial.idx')){
 			dbglog('Creating spatial index');
+			// creates and stores the index
 			$this->generateSpatialIndex();
 			dbglog('Done creating spatial index');
 		} else {
 			dbglog('loading spatial index');
-			$this->$spatial_idx = unserialize(io_readFile($this->idx_dir.'/spatial.idx', false));
-			dbglog($this->$spatial_idx,'done loading spatial index');
+			$this->spatial_idx = unserialize(io_readFile($this->idx_dir.'/spatial.idx', false));
+			dbglog($this->spatial_idx,'done loading spatial index');
 		}
 	}
 
 	/**
-	 * update the spatial index for the page.
-	 * @param string $id the page ID
+	 * Update the spatial index for the page.
+	 * @param string $id the document ID
 	 */
 	function updateSpatialIndex($id) {
 		dbglog('reading getoags','--- spatialhelper_index::updateSpatialIndex ---');
 		$geotags = p_get_metadata($id, 'geo');
-
+		//TODO we need just lat/lon, so check for those
+		//     (and maybe validate the values -90/90 and -180/180)
 		if(empty($geotags)) {
 			dbglog("No geotags found for page $id, done");
 			return true;
@@ -83,40 +80,38 @@ class helper_plugin_spatialhelper_index extends DokuWiki_Plugin {
 		dbglog('Update index for geohash: '.$geohash);
 
 		$pageIds = array();
-		// check index for key
+		// check index for key/geohash
 		if (!array_key_exists($geohash, $this->spatial_idx )) {
 			dbglog('Geohash not in index, just add.');
 			$pageIds[] = $id;
-
 		} else {
 			dbglog('Geohash for document is in index, find it.');
-			// check index for document
+			// check the index for document
 			$knownHashes = $this->findHashesForId($id, $this->spatial_idx);
 			if(empty($knownHashes)){
 				dbglog("No index record found for document $id, just add");
 				$pageIds=$this->spatial_idx[$geohash];
 				$pageIds[] = $id;
 			}
-			
+			// TODO shortcut, need to make sure there is only one element, if not the index is corrupt
 			$knownHash=$knownHashes[0];
 
 			if ($knownHash == $geohash){
-				dbglog('Document '.$id.' was found in index and has the same geohash, done');
+				dbglog("Document $id was found in index and has the same geohash, nothing to do.");
 				return true;
 			}
 
-			
 			if (!empty($knownHash)) {
-				dbglog('Document '.$id.' was found in index but has different geohash (it moved).');
+				dbglog("Document $id was found in index but has different geohash (it moved).");
 				// need to move document to the correct index
 				$knownIds = $this->spatial_idx[$knownHash];
 				dbglog($knownIds,"Known page id's for this hash:");
-
+				// remove it from the old geohash element
 				$i = array_search($id,$knownIds);
 				dbglog('Unsetting:'.$knownIds[$i]);
 				unset($knownIds[$i]);
 				$this->spatial_idx[$knownHash]=$knownIds;
-
+				// set on new geohash element
 				$pageIds=$this->spatial_idx[$geohash];
 				$pageIds[] = $id;
 				dbglog($pageIds,"page id's for this hash:");
@@ -127,6 +122,11 @@ class helper_plugin_spatialhelper_index extends DokuWiki_Plugin {
 		return $this->_saveIndex();
 	}
 
+	/**
+	 * Looks up the geohash(es) for the document in the index.
+	 * @param String $id document ID
+	 * @param array $index spatial index
+	 */
 	function findHashesForId($id,$index) {
 		$hashes=array();
 		foreach ($index as $hash => $docIds){
@@ -136,10 +136,25 @@ class helper_plugin_spatialhelper_index extends DokuWiki_Plugin {
 				$hashes[]=$hash;
 			}
 		}
-		dbglog($hashes,"Found the following hashes for $id");
+		dbglog($hashes,"Found the following hashes for $id (should only be 1)");
 		return $hashes;
 	}
-
+	/**
+	 * Deletes the page from the index.
+	 * @param String $id document ID
+	 */
+	function deleteFromIndex($id){
+		// check the index for document
+		$knownHashes = $this->findHashesForId($id, $this->spatial_idx);
+		// TODO shortcut, need to make sure there is only one element, if not the index is corrupt
+		$knownHash=$knownHashes[0];
+		$knownIds = $this->spatial_idx[$knownHash];
+		$i = array_search($id,$knownIds);
+		dbglog("removing: $knownIds[$i] from the index");
+		unset($knownIds[$i]);
+		$this->spatial_idx[$knownHash]=$knownIds;
+		$this->_saveIndex();
+	}
 	/**
 	 * Save spatial index
 	 */
@@ -149,47 +164,8 @@ class helper_plugin_spatialhelper_index extends DokuWiki_Plugin {
 	}
 
 	/**
-	 * Lock the indexer.
-	 *
-	 * @author Tom N Harris <tnharris@whoopdedo.org>
-	 */
-	private function _lock() {
-		global $conf;
-		$status = true;
-		$run = 0;
-		$lock = $conf['lockdir'].'/_spatial.lock';
-		while (!@mkdir($lock, $conf['dmode'])) {
-			usleep(50);
-			if(is_dir($lock) && time()-@filemtime($lock) > 60*5){
-				// looks like a stale lock - remove it
-				if (!@rmdir($lock)) {
-					$status = "removing the stale lock failed";
-					return false;
-				} else {
-					$status = "stale lock removed";
-				}
-			}elseif($run++ == 1000){
-				// we waited 5 seconds for that lock
-				return false;
-			}
-		}
-		if ($conf['dperm'])
-		chmod($lock, $conf['dperm']);
-		return $status;
-	}
-
-	/**
-	 * Release the indexer lock.
-	 *
-	 * @author Tom N Harris <tnharris@whoopdedo.org>
-	 */
-	private function _unlock() {
-		global $conf;
-		@rmdir($conf['lockdir'].'/_spatial.lock');
-		return true;
-	}
-	/**
-	 * (re-)Generates the spatial index by running through all the pages of the wiki.
+	 * (re-)Generates the spatial index by running through all the pages in the wiki.
+	 * @todo add an option to erase the old index
 	 */
 	function generateSpatialIndex() {
 		global $conf;
