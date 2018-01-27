832bf446e1dcf09539460a56103b2a267314d657
Mark Prins <mprins@users.sf.net>
2018-01-27 14:51:43 +0100

    Russian translations provided by Aleksej Kozlov

---
+++ lang/ru/lang.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Copyright (c) 2018 Mark C. Prins <mprins@users.sf.net>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Russian language file for spatialhelper plugin.
+ *
+ * @license BSD license
+ * @author  Aleksej Kozlov <nellesmeti@gmail.com>
+ */
+$lang ['nothingfound'] = 'Ничего не найдено.';
+$lang ['invalidinput'] = 'Введены некорректные данные.';
+
+$lang ['search_findnearby'] = 'Искать страницы и медиаданные около точки';
+$lang ['search_largerarea'] = 'Расширить область поиска';
+
+$lang ['results_header'] = 'Результаты поиска';
+$lang ['results_pages'] = 'Страницы около точки';
+$lang ['results_media'] = 'Медиаданные около точки';
+$lang ['results_distance_prefix'] = 'расстояние по прямой:&nbsp;';
+$lang ['results_precision'] = 'Погрешность:&nbsp;';
+
+$lang ['admin_submit'] = 'Очистить пространственный индекс';
+$lang ['admin_purged_tiles'] = 'Пространственный индекс был очищен.';
+
+$lang ['menu'] = 'Очистка пространственного индекса';
