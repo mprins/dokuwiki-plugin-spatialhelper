1dbd83b9de2c82a429c356facaea9f9579e5f558
Mark Prins <1165786+mprins@users.noreply.github.com>
2023-12-14 13:24:55 +0100

    Cleanup deprecated API usage

--- helper/index.php
+++ helper/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2011-2022 Mark C. Prins <mprins@users.sf.net>
+ * Copyright (c) 2011-2023 Mark C. Prins <mprins@users.sf.net>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,7 +15,9 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+
 use dokuwiki\Extension\Plugin;
+use dokuwiki\Logger;
 use geoPHP\Geometry\Point;
 
 /**
@@ -31,18 +33,19 @@ class helper_plugin_spatialhelper_index extends Plugin
      *
      * @var string
      */
-    protected $idx_dir = '';
+    protected string $idx_dir = '';
 
     /**
-     * spatial index, well lookup list/array so we can do spatial queries.
+     * spatial index, we'll look up list/array so we can do spatial queries.
      * entries should be: array("geohash" => {"id1","id3",})
      *
      * @var array
      */
-    protected $spatial_idx = [];
+    protected array $spatial_idx = [];
 
     /**
      * Constructor, initialises the spatial index.
+     * @throws Exception
      */
     public function __construct()
     {
@@ -62,16 +65,17 @@ class helper_plugin_spatialhelper_index extends Plugin
                 io_readFile($this->idx_dir . '/spatial.idx', false),
                 ['allowed_classes' => false]
             );
-            dbglog($this->spatial_idx, 'done loading spatial index');
+            Logger::debug('done loading spatial index', $this->spatial_idx);
         }
     }
 
     /**
      * (re-)Generates the spatial index by running through all the pages in the wiki.
      *
+     * @throws Exception
      * @todo add an option to erase the old index
      */
-    public function generateSpatialIndex(): bool
+    final public function generateSpatialIndex(): bool
     {
         global $conf;
         require_once(DOKU_INC . 'inc/search.php');
@@ -98,7 +102,7 @@ class helper_plugin_spatialhelper_index extends Plugin
      *          the document ID
      * @throws Exception
      */
-    public function updateSpatialIndex(string $id): bool
+    final public function updateSpatialIndex(string $id): bool
     {
         $geotags = p_get_metadata($id, 'geo');
         if (empty($geotags)) {
@@ -107,10 +111,10 @@ class helper_plugin_spatialhelper_index extends Plugin
         if (empty($geotags ['lon']) || empty($geotags ['lat'])) {
             return false;
         }
-        dbglog($geotags, "Geo metadata found for page $id");
+        Logger::debug("Geo metadata found for page $id", $geotags);
         $geometry = new Point($geotags ['lon'], $geotags ['lat']);
-        $geohash  = $geometry->out('geohash');
-        dbglog('Update index for geohash: ' . $geohash);
+        $geohash = $geometry->out('geohash');
+        Logger::debug('Update index for geohash: ' . $geohash);
         return $this->addToIndex($geohash, $id);
     }
 
@@ -127,36 +131,36 @@ class helper_plugin_spatialhelper_index extends Plugin
         $pageIds = [];
         // check index for key/geohash
         if (!array_key_exists($geohash, $this->spatial_idx)) {
-            dbglog("Geohash $geohash not in index, just add $id.");
+            Logger::debug("Geohash $geohash not in index, just add $id.");
             $pageIds [] = $id;
         } else {
-            dbglog('Geohash for document is in index, find it.');
+            Logger::debug('Geohash for document is in index, find it.');
             // check the index for document
             $knownHashes = $this->findHashesForId($id, $this->spatial_idx);
             if ($knownHashes === []) {
-                dbglog("No index record found for document $id, just add");
-                $pageIds    = $this->spatial_idx [$geohash];
+                Logger::debug("No index record found for document $id, adding it to the index.");
+                $pageIds = $this->spatial_idx [$geohash];
                 $pageIds [] = $id;
             }
             // TODO shortcut, need to make sure there is only one element, if not the index is corrupt
             $knownHash = $knownHashes [0];
 
             if ($knownHash === $geohash) {
-                dbglog("Document $id was found in index and has the same geohash, nothing to do.");
+                Logger::debug("Document $id was found in index and has the same geohash, nothing to do.");
                 return true;
             }
 
             if (!empty($knownHash)) {
-                dbglog("Document/media $id was found in index but has different geohash (it moved).");
+                Logger::debug("Document/media $id was found in index but has different geohash (it moved).");
                 $knownIds = $this->spatial_idx [$knownHash];
-                dbglog($knownIds, "Known id's for this hash:");
+                Logger::debug("Known id's for this hash:", $knownIds);
                 // remove it from the old geohash element
                 $i = array_search($id, $knownIds);
-                dbglog('Unsetting:' . $knownIds [$i]);
+                Logger::debug('Unsetting:' . $knownIds [$i]);
                 unset($knownIds [$i]);
                 $this->spatial_idx [$knownHash] = $knownIds;
                 // set on new geohash element
-                $pageIds    = $this->spatial_idx [$geohash];
+                $pageIds = $this->spatial_idx[$geohash];
                 $pageIds [] = $id;
             }
         }
@@ -170,18 +174,18 @@ class helper_plugin_spatialhelper_index extends Plugin
      *
      * @param String $id
      *          document ID
-     * @param array  $index
+     * @param array $index
      *          spatial index
      */
-    public function findHashesForId(string $id, array $index): array
+    final public function findHashesForId(string $id, array $index): array
     {
         $hashes = [];
         foreach ($index as $hash => $docIds) {
-            if (in_array($id, $docIds, false)) {
+            if (in_array($id, $docIds)) {
                 $hashes [] = $hash;
             }
         }
-        dbglog($hashes, "Found the following hashes for $id (should only be 1)");
+        Logger::debug("Found the following hashes for $id (should only be 1)", $hashes);
         return $hashes;
     }
 
@@ -204,12 +208,12 @@ class helper_plugin_spatialhelper_index extends Plugin
      * @see http://php.net/manual/en/function.exif-read-data.php
      *
      */
-    public function indexImage($img): bool
+    final  public function indexImage(array $img): bool
     {
         // test for supported files (jpeg only)
         if (
-            (substr($img ['file'], -strlen('.jpg')) !== '.jpg') &&
-            (substr($img ['file'], -strlen('.jpeg')) !== '.jpeg')
+            (!str_ends_with($img ['file'], '.jpg')) &&
+            (!str_ends_with($img ['file'], '.jpeg'))
         ) {
             return false;
         }
@@ -232,7 +236,7 @@ class helper_plugin_spatialhelper_index extends Plugin
      * @return Point|false
      * @throws Exception
      */
-    public function getCoordsFromExif(string $id)
+    final  public function getCoordsFromExif(string $id): Point|false
     {
         $exif = exif_read_data(mediaFN($id), 0, true);
         if (empty($exif ['GPS'])) {
@@ -255,28 +259,29 @@ class helper_plugin_spatialhelper_index extends Plugin
      *
      * @param array $param array of rational DMS
      */
-    public function convertDMStoD(array $param): float
+    final  public function convertDMStoD(array $param): float
     {
-        if (!is_array($param)) {
-            $param = [$param];
-        }
+    //        if (!(is_array($param))) {
+    //            $param = [$param];
+    //        }
         $deg = $this->convertRationaltoFloat($param [0]);
         $min = $this->convertRationaltoFloat($param [1]) / 60;
         $sec = $this->convertRationaltoFloat($param [2]) / 60 / 60;
         // Hemisphere (N, S, W or E)
         $hem = ($param [3] === 'N' || $param [3] === 'E') ? 1 : -1;
+
         return $hem * ($deg + $min + $sec);
     }
 
-    public function convertRationaltoFloat($param): float
+    final public function convertRationaltoFloat(string $param): float
     {
         // rational64u
         $nums = explode('/', $param);
-        if ((int) $nums[1] > 0) {
-            return (float) $nums[0] / (int) $nums[1];
-        } else {
-            return (float) $nums[0];
+        if ((int)$nums[1] > 0) {
+            return (float)$nums[0] / (int)$nums[1];
         }
+
+        return (float)$nums[0];
     }
 
     /**
@@ -284,7 +289,7 @@ class helper_plugin_spatialhelper_index extends Plugin
      *
      * @param string $id document ID
      */
-    public function deleteFromIndex(string $id): void
+    final public function deleteFromIndex(string $id): void
     {
         // check the index for document
         $knownHashes = $this->findHashesForId($id, $this->spatial_idx);
@@ -294,13 +299,12 @@ class helper_plugin_spatialhelper_index extends Plugin
 
         // TODO shortcut, need to make sure there is only one element, if not the index is corrupt
         $knownHash = $knownHashes [0];
-        $knownIds  = $this->spatial_idx [$knownHash];
-        $i         = array_search($id, $knownIds);
-        dbglog("removing: $knownIds[$i] from the index.");
+        $knownIds = $this->spatial_idx [$knownHash];
+        $i = array_search($id, $knownIds);
+        Logger::debug("removing: $knownIds[$i] from the index.");
         unset($knownIds [$i]);
         $this->spatial_idx [$knownHash] = $knownIds;
         if (empty($this->spatial_idx [$knownHash])) {
-            // dbglog ( "removing key: $knownHash from the index." );
             unset($this->spatial_idx [$knownHash]);
         }
         $this->saveIndex();
